a1f3792953ad788b285322d3f784aeb34ca293eb
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-05-24 12:14:24 +0530

    k=int(input()) n=k*k s=b=0 while k!=0:     r=k%10     s=s*10+r     k=k//10 f=s*s while f>0:     j=f%10     b=b*10+j     f=f//10 if b==n:     print(True) else:     print(False)

--- Adam_number.py
+++ Adam_number.py
@@ -1,15 +1,16 @@
-def fun(n):
-    s=0
-    while n!=0:
-        r=n%10
-        s=s*10+r
-        n=n//10
-    return s
-n=int(input())
-m=fun(n)
-s=n*n
-q=m*m
-if fun(q)==s:
+k=int(input())
+n=k*k
+s=b=0
+while k!=0:
+    r=k%10
+    s=s*10+r
+    k=k//10
+f=s*s
+while f>0:
+    j=f%10
+    b=b*10+j
+    f=f//10
+if b==n:
     print(True)
 else:
     print(False)
