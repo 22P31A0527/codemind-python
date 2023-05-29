ee9b6923bb49ebcf8adb344946db8bdfd7650735
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-05-29 16:16:45 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 1     return 0 n=int(input()) c=1  ##already 1 is counted for i in range(2,n+1):     if n%i==0:         if fun(i)==1:             c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,12 +1,12 @@
 def fun(n):
     for i in range(2,n//2+1):
         if n%i==0:
-            return 0
-    return 1
-num=int(input())
-c=1
-for i in range(1,num+1):
-    if num%i==0:
-        if fun(i)==0:
+            return 1
+    return 0
+n=int(input())
+c=1  ##already 1 is counted
+for i in range(2,n+1):
+    if n%i==0:
+        if fun(i)==1:
             c+=1
 print(c)
