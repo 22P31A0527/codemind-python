b51dbdde179911fe9da31d21a899c52c89c29f10
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-05-30 12:02:28 +0530

    m=int(input()) for i in range(1,m+1):     n=int(input())     q=n     s=0     while q!=0:         r=q%10         s=s*10+r         q=q//10     if s==n:         print(True)     else:         print(False)

---
+++ Palindrome_Number.py
@@ -0,0 +1,13 @@
+m=int(input())
+for i in range(1,m+1):
+    n=int(input())
+    q=n
+    s=0
+    while q!=0:
+        r=q%10
+        s=s*10+r
+        q=q//10
+    if s==n:
+        print(True)
+    else:
+        print(False)
