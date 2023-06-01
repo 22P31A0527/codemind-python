7b9f46cc755e24e8031c331285e4ac85ce7797ff
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-01 19:46:21 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:return 0     return 1 n=int(input()) for i in range(1,n+1):     m=int(input())     for i in range(m,2*m+1):         if fun(i)==1:             a=i             break     for i in range(m,0,-1):         if fun(i)==1:             b=i             break     if abs(m-a)==abs(m-b):         print(b)     elif abs(m-a)<abs(m-b):         print(a)     else:         print(b)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -2,19 +2,20 @@ def fun(n):
     for i in range(2,n//2+1):
         if n%i==0:return 0
     return 1
-m=int(input())
-for i in range(1,m+1):
-    n=int(input())
-    for i in range(n,2*n+1):
+n=int(input())
+for i in range(1,n+1):
+    m=int(input())
+    for i in range(m,2*m+1):
         if fun(i)==1:
-            x=i
+            a=i
             break
-    for i in range(n,1,-1):
+    for i in range(m,0,-1):
         if fun(i)==1:
-            y=i
+            b=i
             break
-    if abs(n-x)==abs(n-y):
-        print(y)
-    elif abs(n-x)<abs(n-y):
-        print(x)
-    else:print(y)
+    if abs(m-a)==abs(m-b):
+        print(b)
+    elif abs(m-a)<abs(m-b):
+        print(a)
+    else:
+        print(b)
