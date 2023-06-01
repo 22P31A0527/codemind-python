27772da88b5943970aba45e5d72ace46ea38867b
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-01 17:51:00 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] m=n//2+1 i=0 j=n-1 while i<=m and j>=m:     b.append(a[i])     i+=1     b.append(a[j])     j-=1 if n%2!=0:     b.append(a[i])     print(*b,'0') else:     b.append(a[i])     b.append(a[j])     print(*b)

---
+++ First_and_Last_pair.py
@@ -0,0 +1,18 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=[]
+m=n//2+1
+i=0
+j=n-1
+while i<=m and j>=m:
+    b.append(a[i])   
+    i+=1
+    b.append(a[j])
+    j-=1
+if n%2!=0:
+    b.append(a[i])
+    print(*b,'0')
+else:
+    b.append(a[i])
+    b.append(a[j])
+    print(*b)
