45851e5c93d4ee65cc401e3de24c66e0e94c848d
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-02 09:57:39 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     x=len(str(i))     if x%2==0:         c+=1 print(c)

--- Find_Numbers_with_Even_Number_of_Digits.py
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -1,16 +1,8 @@
-def fun(n):
-    x=0
-    while n!=0:
-        r=n%10
-        x=x+1
-        n=n//10
-    if x%2==0: return 1
-    else:return 0
 n=int(input())
 a=list(map(int,input().split()))
 c=0
 for i in a:
-    p=fun(i)
-    if p==1:
+    x=len(str(i))
+    if x%2==0:
         c+=1
 print(c)
