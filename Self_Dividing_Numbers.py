0cbdde0778b6219e4576361f605d611291a90fe3
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-03 12:22:41 +0530

    def fun(n):     q=n     s=0     while q!=0:         r=q%10         if r==0:             return 0         elif n%r!=0:             return 0         q=q//10     return 1 a=int(input()) b=int(input()) for i in range(a,b+1):     if fun(i)==1:         print(i,end=" ")

---
+++ Self_Dividing_Numbers.py
@@ -0,0 +1,16 @@
+def fun(n):
+    q=n
+    s=0
+    while q!=0:
+        r=q%10
+        if r==0:
+            return 0
+        elif n%r!=0:
+            return 0
+        q=q//10
+    return 1
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if fun(i)==1:
+        print(i,end=" ")
