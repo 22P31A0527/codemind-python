27912c1f1202d0abaa5285e57a597d70f5974f48
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-05 19:42:21 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) if fun(n)==1:     print('prime') else:     print('not a prime')

--- prime_number.py
+++ prime_number.py
@@ -1,7 +1,10 @@
+def fun(n):
+    for i in range(2,n//2+1):
+        if n%i==0:
+            return 0
+    return 1
 n=int(input())
-for i in range(2,n//2+1):
-    if n%i==0:
-        print('not a prime')
-        break
-else:
+if fun(n)==1:
     print('prime')
+else:
+    print('not a prime')
