ecbdc0ca178e3482f8bb27d3f9d4871d275a058c
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-05 19:59:56 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) for i in range(n,2*n+1):     if fun(i)==1:         p=i         break for i in range(n,1,-1):     if fun(i)==1:         q=i         break if abs(p-n)==abs(q-n):     print(abs(p-n)) elif abs(p-n)>abs(q-n):     print(abs(q-n)) else:     print(abs(p-n))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,19 +1,20 @@
 def fun(n):
     for i in range(2,n//2+1):
-        if n%i==0:return 0
+        if n%i==0:
+            return 0
     return 1
 n=int(input())
 for i in range(n,2*n+1):
     if fun(i)==1:
-        a=i
+        p=i
         break
-for i in range(n,0,-1):
+for i in range(n,1,-1):
     if fun(i)==1:
-        b=i
-        break
-if abs(n-a)==abs(n-b):
-    print(abs(n-a))
-elif abs(n-a)<abs(n-b):
-    print(abs(n-a))
+        q=i
+        break 
+if abs(p-n)==abs(q-n):
+    print(abs(p-n))
+elif abs(p-n)>abs(q-n):
+    print(abs(q-n))
 else:
-    print(abs(n-b))
+    print(abs(p-n))
