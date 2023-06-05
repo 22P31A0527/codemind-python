fa27150dcebb273b1465941dddc25ca13e84d1d2
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-05 19:47:17 +0530

    def fun(n):     for i in range(2,n//2+1):         if n%i==0:             return 0     return 1 n=int(input()) for i in range(2,n//2+1):     if n%i==0:         if fun(i)==1:             n1=i             n2=n//i             if fun(n2)==1:                 print(n1,n2)                 break else:     print('-1')

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -4,15 +4,13 @@ def fun(n):
             return 0
     return 1
 n=int(input())
-s=0
-for i in range(2,n):
+for i in range(2,n//2+1):
     if n%i==0:
-        x=i
-        if fun(x)==1:
-            y=n//x
-            if fun(y)==1:
-                print(x,y)
-                s=1
+        if fun(i)==1:
+            n1=i
+            n2=n//i
+            if fun(n2)==1:
+                print(n1,n2)
                 break
-if s==0:
+else:
     print('-1')
