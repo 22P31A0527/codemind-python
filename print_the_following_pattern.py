642526bdf0d649129ec491cdf5142ff7b533ebd3
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-08 13:55:29 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,i+1):         if j==i or i==1 or i==n or j==1:             print("*",end="")         else:             print(" ",end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,8 @@
 n=int(input())
-x=ord('A')
-for i in range(n,0,-1):
-    for j in range(i,0,-1):
-        print(chr(i+x-1),end=" ")
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if j==i or i==1 or i==n or j==1:
+            print("*",end="")
+        else:
+            print(" ",end="")
     print()
