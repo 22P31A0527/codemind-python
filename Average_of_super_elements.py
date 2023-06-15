430d779625ea94936490a9f65380580a19ea9d2a
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-06-15 19:23:40 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in range(0,n):     x=a.count(a[i])     if x==a[i]:         if a[i] not in b:             b.append(a[i]) if len(b)!=0:     av=sum(b)/len(b)     print(format(av,".2f")) else:     print('-1')

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,14 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=[]
-s=0
-for i in a:
-    if i==a.count(i):
-        if i not in b:
-            b.append(i)
-            s=s+i
-if s>0:
-    t=s/len(b)
-    print(format(t,".2f"))
+for i in range(0,n):
+    x=a.count(a[i])
+    if x==a[i]:
+        if a[i] not in b:
+            b.append(a[i])
+if len(b)!=0:
+    av=sum(b)/len(b)
+    print(format(av,".2f"))
 else:
     print('-1')
