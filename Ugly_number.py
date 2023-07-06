61f152c4aff45b97ad0a6fe24ceefffc68ce7b37
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-07-06 12:22:32 +0530

    n=int(input()) s=1 while n!=1:     if n%2==0:         n=n//2     elif n%3==0:         n=n//3     elif n%5==0:         n=n//5     else:         s=0         break if s==1:     print('Ugly Number') else:     print('Not Ugly Number')

--- Ugly_number.py
+++ Ugly_number.py
@@ -1,17 +1,16 @@
 n=int(input())
-i=0
 s=1
 while n!=1:
     if n%2==0:
-        n=n/2
+        n=n//2
     elif n%3==0:
-        n=n/3
+        n=n//3
     elif n%5==0:
-        n=n/5
+        n=n//5
     else:
         s=0
         break
-if s==0:
-    print("Not Ugly Number")
+if s==1:
+    print('Ugly Number')
 else:
-    print("Ugly Number")
+    print('Not Ugly Number')
