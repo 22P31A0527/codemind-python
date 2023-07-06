38a3425531b38d722c906a665132eb1f6c0930d7
22P31A0527 <119730531+22P31A0527@users.noreply.github.com>
2023-07-06 20:45:06 +0530

    s=input() s=s.split() v='aeiou' l=[] for i in range(0,len(s)):     x=s[i]     c=0     for i in range(0,len(x)):         if x[i] in v:             c+=1     l.append(c) m=min(l) print(l.count(m))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,13 @@
+s=input()
+s=s.split()
+v='aeiou'
+l=[]
+for i in range(0,len(s)):
+    x=s[i]
+    c=0
+    for i in range(0,len(x)):
+        if x[i] in v:
+            c+=1
+    l.append(c)
+m=min(l)
+print(l.count(m))
